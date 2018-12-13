08f54335c99220cb347b577e5e22af42f7f49cfd
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-13 18:06:20 +0530

    Delete and Priority merged

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:keepr/models/note.dart';
 import 'package:keepr/screens/note_details.dart';
+import 'package:keepr/utils/db_helper.dart';
 
 class NoteList extends StatefulWidget {
   @override
@@ -9,9 +11,15 @@ class NoteList extends StatefulWidget {
 }
 
 class NoteListState extends State<NoteList> {
+  DatabaseHelper dbHelper = DatabaseHelper();
+  List<Note> noteList;
   int count = 0;
+
   @override
     Widget build(BuildContext context) {
+      if (noteList == null) {
+        noteList = List<Note>();
+      }
 
       return Scaffold(
         appBar: AppBar(
@@ -41,17 +49,44 @@ class NoteListState extends State<NoteList> {
             color: Colors.black,
             elevation: 2.0,
             child: ListTile(
-              leading: Icon(Icons.alarm_on),
-              title: Text('Title', style: titleStyle),
-              subtitle: Text('Reason'),
-              trailing: Icon(Icons.delete_sweep),
+              leading: CircleAvatar(
+                backgroundColor: getPriorityRang(this.noteList[position].priority),
+                child: Icon(Icons.alarm_on),
+              ),
+              title: Text(this.noteList[position].title, style: titleStyle),
+              subtitle: Text(this.noteList[position].description),
+              trailing: GestureDetector (
+                child: Icon(Icons.delete_sweep, color: Colors.red),
+                onTap: () {
+                  _delete(context, noteList[position]);
+                },
+              ),
               onTap: () {
-                navigateToDetail('Edit Note');
+                debugPrint("Edit Check");
               },
             ),
           );
         },
       );
+    }
+
+    void _delete(BuildContext context, Note note) async {
+      int res = await dbHelper.deleteNote(note);
+      if (res != 0) {
+        _showSnackBar(context, 'Note Deleted!');
+      }
+    }
+
+    void _showSnackBar(BuildContext context, String msg) {
+      final snackBar = SnackBar(content: Text(msg));
+      Scaffold.of(context).showSnackBar(snackBar);
+    }
+
+    Color getPriorityRang(int priority) {
+      if (priority == 1) {
+        return Colors.red;
+      }
+      return Colors.yellow;
     }
 
     void navigateToDetail(String title) {
